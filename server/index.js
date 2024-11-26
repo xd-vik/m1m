d18b9a9c23cc35d9c4f12e4a7720479d02abb625
Vivek <vivekcodzz@gmail.com>
2024-11-26 10:18:05 +0530

    routes Configured

--- server/index.js
+++ server/index.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
+const allowedOrigin = process.env.ORIGIN || "*";
+const DB_URI = process.env.DB_URI;
+
+app.use(
+  cors({
+    origin: allowedOrigin,
+  })
+);
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello From Backend!");
 });
+
+app.get("/test", (req, res) => {
+  res.send("good to go");
+});
+
+mongoose
+  .connect(DB_URI)
+  .then(() => console.log("Database is Connected"))
+  .catch((e) => console.log(e));
 
 app.listen(PORT, () => {
   console.log(`server is running at http://localhost:${PORT}`);
