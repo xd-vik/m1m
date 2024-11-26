c97a852245a9715b5aaed8dfc66266e72a1ff7a2
strivervishal <strivervishal@gmail.com>
2024-11-26 09:27:38 +0530

    router-setup

---
+++ client/src/Components/AboutUs.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const AboutUs = () => {
+  return <Link to="/about">AboutUs</Link>;
+};
+
+export default AboutUs;
---
+++ client/src/Components/SignUp.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SignUp = () => {
+  return <Link><div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa perferendis, distinctio blanditiis nemo omnis culpa rem perspiciatis dicta accusantium sit laudantium voluptates, similique quia! Adipisci praesentium iste tenetur quod. Quidem aut mollitia doloremque ex earum iste tempore soluta, quibusdam repellendus odio quo. Corporis saepe cumque provident voluptate amet beatae sed cupiditate consequatur eligendi quasi facere quae maiores, enim est voluptates. Veniam eius illo perferendis quisquam eligendi sed asperiores magni explicabo architecto ut, voluptas suscipit omnis repudiandae enim provident, amet aperiam eveniet, accusantium placeat. Iure dignissimos consequatur molestias delectus vero nisi. Illum officia deserunt accusantium quaerat nam, repellat perspiciatis iste natus!</div></Link>;
+};
+
+export default SignUp;
