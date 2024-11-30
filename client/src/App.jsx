3b5e68fec3de8a67cdd6ff86128eb61baab91775
Aman Singh <singhamanrajput10@gmail.com>
2024-12-01 00:41:40 +0530

    Footer completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./Components/Login";
 import SignUp from "./Components/SignUp";
 import AboutUs from "./Components/AboutUs";
 import HomePage from "./Pages/HomePage";
+import Footer from "./Components/Footer";
 import Router from "./React-Router/Router";
 function App() {
   return (
--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import NavBar from '../Components/NavBar'
 import Hero from '../Components/Hero'
 import AboutUs from '../Components/AboutUs'
+import Footer from  '../Components/Footer'
 const HomePage = () => {
   return (
     <div className='min-h-screen w-full font-alexandria'>
       <NavBar/>
       <Hero/>
       <AboutUs/>
+      <Footer/>
     </div>
   )
 }
