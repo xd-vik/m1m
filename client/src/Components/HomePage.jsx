62b66153e3b7221c3bd444c51fd9925e6026c462
Manish Nagar <mnagargujjar33@gmail.com>
2024-11-30 21:36:37 +0530

    Login/Signup

--- client/src/Components/HomePage.jsx
+++ client/src/Components/HomePage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import logo from '../../public/images/logom1m.jpg'
 import { Link } from 'react-router-dom'
+import Login from './Login';
+import Signup from './SignUp';
 
 const HomePage = () => {
   return (
@@ -17,8 +19,8 @@ const HomePage = () => {
             <Link className='p-5 m-2'>Contact Us</Link>
           </div>
           <div className='w-[20%] flex justify-center items-center gap-10 h-[10vh]'>
-            <Link className='px-5 py-2 rounded-lg text-[1.34rem] text-[#015DFE] border-2 border-[#015DFE] font-semibold'>Sign-in</Link>
-            <Link className='px-5 py-2 text-[1.34rem] bg-[#015DFE] text-white rounded-lg font-semibold'>Login</Link>
+            <Link to='/Signup ' className='px-5 py-2 rounded-lg text-[1.34rem] text-[#015DFE] border-2 border-[#015DFE] font-semibold'>Sign-in</Link>
+            <Link  to='/Login '  className='px-5 py-2 text-[1.34rem] bg-[#015DFE] text-white rounded-lg font-semibold'>Login</Link>
           </div>
         </div>
       </nav>
