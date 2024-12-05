b0807989958e334032660c83ecbde7014ed7a446
Manish Nagar <mnagargujjar33@gmail.com>
2024-12-05 17:24:51 +0530

    Responsive

--- client/src/Components/Work.jsx
+++ client/src/Components/Work.jsx
@@ -67,10 +67,10 @@ const Work=()=>{
                 </div>
             </div>
 
-        <div className='w-[90%] sm:h-[110vh] md:h-[80vh] lg:h-[80vh] xl:h-[50vh] h-[190vh] ml-[5%]'>
->>>>>>>
+        <div className='p-8'>
+
                 <h1 className='text-[3.5em] font-semibold text-[#122766]'>Why Choose Us?</h1>
-                <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-between h-[45vh] py-1'>
+                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4'>
                     {ChooseData.map((data,index)=>{
                         return(
                             <ChooseCard data={data} key={index}/>
