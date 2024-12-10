14f563ad1b99a7b9d73fb46891469e8cec27c11a
Vivek <vivekcodzz@gmail.com>
2024-12-10 13:41:33 +0530

    rollbacked safeln

---
+++ server/Models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ["student", "instructor", "admin"],
+    required: true,
+  },
+  profile_picture: { type: String },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("User", UserSchema);
